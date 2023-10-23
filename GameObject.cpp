1a8275a2ab8b9ccbe4518fc46cc5d02b5a00891a
yakisake <houd724@yahoo.co.jp>
2023-10-23 21:24:06 +0900

    GameObjectを追加

---
+++ GameObject.cpp
@@ -0,0 +1,89 @@
+﻿#include "GameObject.hpp"
+
+GameObject::GameObject(const Vec2& pos, const SizeF& hitSize, const SizeF& viewSize)
+{
+	m_pos = pos;
+	m_hitSize = hitSize;
+	m_viewSize = viewSize;
+	m_isAlive = true;
+}
+
+GameObject::GameObject(const Vec2& pos, const SizeF& size)
+{
+	m_pos = pos;
+	m_hitSize = size;
+	m_viewSize = size;
+	m_isAlive = true;
+}
+
+
+void GameObject::moveX(const double& x)
+{
+	m_pos.x += x;
+}
+
+void GameObject::moveY(const double& y)
+{
+	m_pos.y += y;
+}
+
+
+bool GameObject::isHit(const GameObject& object) const
+{
+	return hitRegion().intersects(object.hitRegion());
+}
+
+
+Vec2 GameObject::pos() const
+{
+	return m_pos;
+}
+
+RectF GameObject::hitRegion() const
+{
+	const double x = m_pos.x - (m_hitSize.x / 2);
+	const double y = m_pos.y - m_hitSize.y;
+
+	return RectF{ x, y, m_hitSize };
+}
+
+RectF GameObject::viewRegion() const
+{
+	// viewRegion() の中央は hitRegion() の中央
+
+	const double x = m_pos.x - (m_viewSize.x / 2);
+	const double y = m_pos.y - m_viewSize.y + (m_viewSize.y - m_hitSize.y) / 2;
+
+	return RectF{ x, y, m_viewSize };
+}
+
+double GameObject::leftX() const
+{
+	return hitRegion().leftX();
+}
+
+double GameObject::rightX() const
+{
+	return hitRegion().rightX();
+}
+
+double GameObject::topY() const
+{
+	return hitRegion().topY();
+}
+
+double GameObject::bottomY() const
+{
+	return hitRegion().bottomY();
+}
+
+
+bool GameObject::isAlive() const
+{
+	return m_isAlive;
+}
+
+void GameObject::kill()
+{
+	m_isAlive = false;
+}
---
+++ GameObject.hpp
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+class GameObject
+{
+public:
+	GameObject(const Vec2& pos, const SizeF& hitSize, const SizeF& viewSize);
+	GameObject(const Vec2& pos, const SizeF& size);
+	virtual ~GameObject() = default;
+
+	virtual void update() = 0;
+	virtual void draw() const = 0;
+
+	void moveX(const double& x);
+	void moveY(const double& y);
+
+	bool isHit(const GameObject& object) const;
+
+	Vec2 pos() const;
+	RectF hitRegion() const;
+	RectF viewRegion() const;
+	double leftX() const;
+	double rightX() const;
+	double topY() const;
+	double bottomY() const;
+
+	bool isAlive() const;
+	void kill();
+
+	virtual void pause() = 0;
+	virtual void resume() = 0;
+
+
+protected:
+	Texture m_texture;
+	// 中央下の座標
+	Vec2 m_pos;
+	Vec2 m_hitSize;
+	Vec2 m_viewSize;
+	bool m_isAlive;
+};
