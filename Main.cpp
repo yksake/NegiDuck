20503d10b170a7375308ab2ae738c66ec0265fb4
yakisake <houd724@yahoo.co.jp>
2023-10-23 21:01:28 +0900

    1st

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+﻿# include <Siv3D.hpp> // Siv3D v0.6.12
+
+void Main()
+{
+	Window::SetTitle(U"レンゲ背負ってやってきた！");
+	Window::ResizeActual(DisplayResolution::HD_1280x720);
+	Scene::SetResizeMode(ResizeMode::Actual);
+	Scene::SetBackground(Color{ 34, 32, 52 });
+
+	while (System::Update())
+	{
+		const ScopedRenderStates2D sampler{ SamplerState::ClampNearest };
+	}
+}
