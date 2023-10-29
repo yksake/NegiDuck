3598484c7b070e64d23c0d7e08dc9c9c660ca500
yakisake <houd724@yahoo.co.jp>
2023-10-29 23:07:04 +0900

    フォルダを整理

---
+++ NegiDuck/Crypto/Crypto.hpp
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+#include <secblock.h>
+
+class Crypto
+{
+public:
+	static std::string Encrypt(const std::string& plain);
+	static std::string Decrypt(const std::string& cipher);
+
+
+private:
+	Crypto();
+
+	static Optional<CryptoPP::SecByteBlock> GetKey();
+};
